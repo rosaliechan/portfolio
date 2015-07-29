915f0cce6f7c8c2fb6bfce85f2bd7146ba8550e9
rosaliechan <rosaliemchan@yahoo.com>
2015-07-29 10:11:10 -0400

    portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'pages#welcome'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/portfolio' => 'pages#portfolio'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
